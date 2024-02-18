dd2614265145fa733f63ec3aa2d1ae02fbbb7383
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-02-18 18:14:54 +0530

    Add files via upload

---
+++ src/app/Components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center items-center w-screen h-[70vh]'>
+    LOADING...
+  </div> 
+      
+  )
+}
+
+export default Loading
