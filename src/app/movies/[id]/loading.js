5df7def786687331cd5db14b94876c68607e43fd
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-02-18 18:02:54 +0530

    Update loading.js

--- src/app/movies/[id]/loading.js
+++ src/app/movies/[id]/loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '@/app/Components/Header'
 const loading = () => {
   return (
-    <div className='flex justify-center items-center w-screen h-screen'>
+    <div className='flex justify-center items-center w-screen h-[60%]'>
     LOADING...
   </div> 
       
