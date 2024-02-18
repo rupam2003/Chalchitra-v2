178b8f24d8c3694a021675233fc9c938dd3f64e5
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-02-18 18:16:28 +0530

    Update loading.js

--- src/app/search/[id]/loading.js
+++ src/app/search/[id]/loading.js
@@ -1,11 +1,9 @@
+import Loading from '@/app/Components/Loading'
 import React from 'react'
-import Header from '@/app/Components/Header'
+
 const loading = () => {
   return (
-    <div className='flex justify-center items-center w-screen h-screen'>
-      LOADING...
-    </div>  
-      
+    <Loading/>
   )
 }
 
