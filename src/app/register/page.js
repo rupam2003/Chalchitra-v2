a6beab5117d7c41eeaf7177c1f1def94e1c7ee55
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-02-20 14:41:09 +0530

    Update page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -16,7 +16,7 @@ const Page = () => {
         
         if(!name||!email||!password)
           {
-            seterror("all fields required")
+            seterror("All fields required")
             return
           }
           const data = await fetch("api/userExists" , {
@@ -54,15 +54,18 @@ const Page = () => {
 
     }
   return (
-    <div className='h-screen grid place-items-center'>
-        <form onSubmit={handleSubmit} className='text-black flex flex-col'>
-            <input onChange={(e)=>{setname(e.target.value)}} className='p-1.5 font-semibold rounded-md w-[400px] my-2 bg-gray-300' type='text' placeholder='name' value={name}/>
-            <input onChange={(e)=>{setemail(e.target.value)}} className='p-1.5 font-semibold rounded-md w-[400px] my-2 bg-gray-300' type='text' placeholder='email' value={email} />
-            <input onChange={(e)=>{setpasword(e.target.value)}} className='p-1.5 font-semibold rounded-md w-[400px] my-2 bg-gray-300' type='text' placeholder='password' value={password}/>
-            <button  className='bg-green-500 p-1.5 font-bold rounded-md w-[400px] my-2' >REGISTER</button>
+   <div className='flex flex-col items-center'>
+        <form onSubmit={handleSubmit} className='mt-[25vh] text-black flex flex-col'>
+            <input onChange={(e)=>{setname(e.target.value)}} className='py-1.5 px-3 outline-none font-semibold rounded-xl w-[300px] my-2 bg-opacity-25 text-white bg-gray-300' type='text' placeholder='Name' value={name}/>
+            <input onChange={(e)=>{setemail(e.target.value)}} className='py-1.5 px-3 outline-none font-semibold rounded-xl w-[300px] my-2 bg-opacity-25 text-white bg-gray-300' type='text' placeholder='Email' value={email} />
+            <input onChange={(e)=>{setpasword(e.target.value)}} className='py-1.5 px-3 outline-none font-semibold rounded-xl w-[300px] my-2 bg-opacity-25 text-white bg-gray-300' type='password' placeholder='Password' value={password}/>
+            <button  className='bg-green-500 p-1.5 font-bold rounded-lg  my-2' >REGISTER</button>
         </form>
-       <h1 className=' bg-red-600'>{error}</h1>
-       <Link href={"/login"}>login</Link>
+       <h1>Already have an account?
+       <Link className=' text-green-600' href={"/login"}> Login</Link>
+       </h1>
+       
+       <h1 className='px-2 mt-2 rounded-lg bg-red-600'>{error}</h1>
 
     </div>
   )
