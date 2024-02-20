714eae1bb69bc9dd813e8b6d4b546019082b0af3
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-02-20 17:42:59 +0530

    Update Loading.js

--- src/app/Components/Loading.js
+++ src/app/Components/Loading.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Loading = () => {
   return (
+    
     <div className='flex justify-center items-center w-screen h-[70vh]'>
-    LOADING...
-  </div> 
+      <div className='dot'></div>
+      <div className='dot'></div>
+      <div className='dot'></div>
+
+  </div>
+  
+  
+
       
   )
 }
